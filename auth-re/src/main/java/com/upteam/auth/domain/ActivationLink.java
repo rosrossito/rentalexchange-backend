e8da29a43965b026683ab0ee57e2179a31aa8712
Ruslan <o_rusik2008@mail.ru>
2015-12-05 14:52:22 +0200

    project structure

--- auth-re/src/main/java/com/upteam/auth/domain/ActivationLink.java
+++ auth-re/src/main/java/com/upteam/auth/domain/ActivationLink.java
@@ -1,8 +1,45 @@
 package com.upteam.auth.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by opasichnyk on 12/4/2015.
  */
 //Entity
 public class ActivationLink {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    String linkUUID;
+    long systemuser_id;
+    enum type {confirmRegistration, restorePassword};
+
+    public ActivationLink() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLinkUUID() {
+        return linkUUID;
+    }
+
+    public void setLinkUUID(String linkUUID) {
+        this.linkUUID = linkUUID;
+    }
+
+    public long getSystemuser_id() {
+        return systemuser_id;
+    }
+
+    public void setSystemuser_id(long systemuser_id) {
+        this.systemuser_id = systemuser_id;
+    }
 }
--- auth-re/src/main/java/com/upteam/auth/domain/SystemUser.java
+++ auth-re/src/main/java/com/upteam/auth/domain/SystemUser.java
@@ -1,8 +1,65 @@
 package com.upteam.auth.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by opasichnyk on 11/25/2015.
  */
 //Entity
+
+@Entity
 public class SystemUser {
+
+@Id
+@GeneratedValue
+private Long id;
+
+String login;
+String email;
+String image;
+enum status {temporary, active, blocked, delete};
+String password;
+
+    public SystemUser() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
