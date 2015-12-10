50640a53247d462572f018f86a635c53d0824e6e
Kostik <kostik.molodoi@gmail.com>
2015-12-10 18:05:55 +0200

    Add Exception Config

---
+++ auth-re/src/main/java/com/upteam/auth/config/ExceptionConfig.java
@@ -0,0 +1,12 @@
+package com.upteam.auth.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Skirdovs on 10.12.2015.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.upteam.auth.exception")
+public class ExceptionConfig {
+}
