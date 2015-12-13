ed4820dc0e986dcade73e4bb8a6f48986a1c9d03
Maxim Maximov <mpmaximov@gmail.com>
2015-12-13 20:42:04 +0200

    Clear from comments

--- auth-re/src/main/java/com/upteam/auth/config/PersistanceConfig.java
+++ auth-re/src/main/java/com/upteam/auth/config/PersistanceConfig.java
@@ -29,75 +29,8 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-//@ComponentScan(basePackageClasses = com.upteam.auth.domain.SystemUser.class)//basePackages = "com.upteam.auth.domain.SystemU")
-//@PropertySource(value = {"classpath:application.properties"})
-//@EnableTransactionManagement
-//@EnableJpaRepositories(basePackages = "com.upteam.auth.repository")
 @EntityScan(basePackageClasses= SystemUser.class)
 public class PersistanceConfig {
 
-/*    @Autowired
-    private Environment env;
-
-    @Autowired
-    DataSource datasource;
-
-    @org.springframework.beans.factory.annotation.Value("${init-db:false}")
-    private String initDatabase;
-
-
-    @Bean
-    public PlatformTransactionManager transactionManager()
-    {
-        EntityManagerFactory factory = entityManagerFactory().getObject();
-        return new JpaTransactionManager(factory);
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory()
-    {
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(Boolean.TRUE);
-        vendorAdapter.setShowSql(Boolean.TRUE);
-
-        factory.setDataSource(datasource);
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.upteam.auth.domain");
-
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-        factory.setJpaProperties(jpaProperties);
-
-        factory.afterPropertiesSet();
-        factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
-        return factory;
-    }
-
-    @Bean
-    public DataSource dataSource()
-    {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        return dataSource;
-    }
-
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(DataSource dataSource)
-    {
-        System.out.println("**************************"+initDatabase);
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(dataSource);
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-        databasePopulator.addScript(new ClassPathResource("db.sql"));
-        dataSourceInitializer.setDatabasePopulator(databasePopulator);
-        dataSourceInitializer.setEnabled(Boolean.parseBoolean(initDatabase));
-        return dataSourceInitializer;
-    }
-*/
 
 }
