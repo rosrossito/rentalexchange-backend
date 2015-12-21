e131bea6caae468b3c85b1ebed662c85bfd52527
olegls2000 <pasechnik.o.n@gmail.com>
2015-12-21 17:31:19 +0200

    add template velocity

--- auth-re/src/main/java/com/upteam/auth/controller/AuthController.java
+++ auth-re/src/main/java/com/upteam/auth/controller/AuthController.java
@@ -4,10 +4,19 @@ import com.upteam.auth.exception.UserAlreadyExistException;
 import com.upteam.auth.service.AuthService;
 import com.upteam.auth.vo.RegistrationConfirmRequestVO;
 import com.upteam.auth.vo.RegistrationRequestVO;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by opasichnyk on 11/25/2015.
@@ -30,4 +39,54 @@ public class AuthController {
     void userRegistrationConfirm(@RequestBody RegistrationConfirmRequestVO request)  {
         authService.confirmRegistration(request);
     }
+
+//-------------------- Test method: ---------------------
+
+    @Autowired
+    private VelocityEngine velocityEngine;
+    @Autowired
+    private JavaMailSenderImpl javaMailSender;
+
+    @RequestMapping(value = "/email-template", method = RequestMethod.GET)
+    @ResponseBody
+    void sendEmailTemplate()  {
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+                message.setTo("pasechnik.o.n@gmail.com");
+                message.setFrom("exchange.rental.info@gmail.com");
+                Map model = new HashMap();
+                User user = new User();
+                user.setEmailAddress("emailFromUserObject@gmail.com");
+                user.setUserName("userNameFromUserObject");
+                model.put("user", user);
+                String text = VelocityEngineUtils.mergeTemplateIntoString(
+                        velocityEngine, "com/temp.vm", "UTF-8", model);
+                message.setText(text, true);
+            }
+        };
+        this.javaMailSender.send(preparator);
+    }
+
+    private class User {
+        private String userName;
+        private String emailAddress;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getEmailAddress() {
+            return emailAddress;
+        }
+
+        public void setEmailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+        }
+    }
+
 }
