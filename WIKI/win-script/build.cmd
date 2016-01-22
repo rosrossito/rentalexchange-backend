8e6e1d48847252abdb193a582b1ad0960c3df5a9
olegls2000 <pasechnik.o.n@gmail.com>
2016-01-22 16:04:08 +0200

    Change scripts.

--- WIKI/win-script/build.cmd
+++ WIKI/win-script/build.cmd
@@ -6,4 +6,4 @@ if not defined BUILD_BACKEND_LOG_FILE set BUILD_BACKEND_LOG_FILE=%LOG_DIR%\build
 echo Start build..
 echo Logging into %BUILD_BACKEND_LOG_FILE%
 cd rentalexchange-backend                                                                   
-mvn -gs ../maven/settings.xml -PliquibaseProps -PdeployDBSchema clean install >  %BUILD_BACKEND_LOG_FILE% 2>&1
+mvn -gs D:/Oleg/rentalexchange-backend/WIKI/maven/settings.xml -PliquibaseProps -PdeployDBSchema clean install >  %BUILD_BACKEND_LOG_FILE% 2>&1
