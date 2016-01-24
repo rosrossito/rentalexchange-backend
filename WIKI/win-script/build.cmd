334db7ec11f44e6dc305d427ea859d43b102f0a0
olegls2000 <pasechnik.o.n@gmail.com>
2016-01-24 23:20:05 +0200

    Изменена система запуска проекта.

--- WIKI/win-script/build.cmd
+++
@@ -1,9 +0,0 @@
-rem INSTRUCTION:
-rem instad of '-gs WIKI/maven/settings.xml' - setup path to settings.xml stored on instance
-call setvars.cmd
-
-if not defined BUILD_BACKEND_LOG_FILE set BUILD_BACKEND_LOG_FILE=%LOG_DIR%\build-backend.log
-echo Start build..
-echo Logging into %BUILD_BACKEND_LOG_FILE%
-cd rentalexchange-backend                                                                   
-mvn -gs D:/Oleg/rentalexchange-backend/WIKI/maven/settings.xml -PliquibaseProps -PdeployDBSchema clean install >  %BUILD_BACKEND_LOG_FILE% 2>&1
