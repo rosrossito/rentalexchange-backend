8c3bf92c276151422388a5b2220d75c6af1ebfc2
Nasadiuk <yeapless@gmail.com>
2016-02-05 23:02:34 +0200

    security first step

---
+++ auth-re/src/main/java/com/upteam/auth/security/SpringWebMvcInitializer.java
@@ -0,0 +1,25 @@
+package com.upteam.auth.security;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by Vlad on 05.02.2016.
+ */
+public class SpringWebMvcInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { CustomWebSecurityConfigurerAdapter.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
