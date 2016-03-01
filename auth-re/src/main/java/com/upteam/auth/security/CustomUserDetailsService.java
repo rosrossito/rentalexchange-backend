599eb84346ecc759be6b46aee057fc01645b183f
Maxim Maximov <mpmaximov@gmail.com>
2016-03-01 22:20:47 +0200

    Security for REST working with login and email (cherry picked from commit 2abd342)

--- auth-re/src/main/java/com/upteam/auth/security/CustomUserDetailsService.java
+++ auth-re/src/main/java/com/upteam/auth/security/CustomUserDetailsService.java
@@ -7,10 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import sun.plugin.util.UserProfile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,18 +28,23 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
         SystemUser user = userService.searchByLogin(s);
-        System.out.println("User : " + user);
-        if (user == null) {
-            LOG.warn("User with login:" + s + " not found");
+        if (user==null) user = userService.searchByEmail(s);
+        System.out.println("User : "+user);
+        if(user==null){
+            System.out.println("User not found");
             throw new UsernameNotFoundException("Username not found");
         }
         return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
                 user.getStatus().equals(SystemUserStatus.active), true, true, true, getGrantedAuthorities(user));
+
+
     }
 
-    private List<GrantedAuthority> getGrantedAuthorities(SystemUser user) {
+    private List<GrantedAuthority> getGrantedAuthorities(SystemUser user){
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
 /*        for(UserProfile userProfile : user.getUserProfiles()){
             System.out.println("UserProfile : "+userProfile);
             authorities.add(new SimpleGrantedAuthority("ROLE_"+userProfile.getType()));
